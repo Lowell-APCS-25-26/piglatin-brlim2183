65f2035d17dfdc16d926dea20c5b817177790586
brlim2183 <brlim2183@gmail.com>
2026-01-21 12:53:00 -0800

    Fix findFirstVowel method to correctly identify vowels
    Corrected the loop variable and fixed the method to check for vowels in the input string.

--- lib/src/main/java/org/APCSLowell/PigLatin.java
+++ lib/src/main/java/org/APCSLowell/PigLatin.java
@@ -4,7 +4,18 @@ public class PigLatin {
     public int findFirstVowel(String sWord) {
         //precondition: sWord is a valid String of length greater than 0.
         //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
-	    
+	    for(int i=0; i<word.length();i++){
+    if(sWord.substring(i,i+1).equals("a"))
+    return i;
+    if(sWord.substring(i,i+1).equals("e"))
+    return i;
+    if(sWord.substring(i,i+1).equals("i"))
+    return i;
+    if(sWord.substring(i,i+1).equals("u"))
+    return i;
+    if(sWord.substring(i,i+1).equals("o"))
+    return i;
+  }
         return -1;
     }
 
