d0ae644b23ce7b470ad2620adc276ff01572f931
brlim2183 <brlim2183@gmail.com>
2026-01-23 00:47:11 -0800

    Update PigLatin.java

--- lib/src/main/java/org/APCSLowell/PigLatin.java
+++ lib/src/main/java/org/APCSLowell/PigLatin.java
@@ -22,7 +22,11 @@ public class PigLatin {
     public String pigLatin(String sWord) {
         //precondition: sWord is a valid String of length greater than 0
         //postcondition: returns the pig latin equivalent of sWord
-        
+		for (int i=0; i<sWord.length(); i++){
+			if (findFirstVowel(sWord)==0)
+				return sWord + "way";
+			return (sWord.substring(findFirstVowel(sWord),sWord.length())+sWord.substring(0,findFirstVowel(sWord))+"ay");
+		}
         return null;
     }
 }
